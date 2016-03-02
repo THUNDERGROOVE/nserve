54f967c4901b6859d4c122b1d63671ede74d52a8
Nick Powell <markusthefish@gmail.com>
2016-03-01 19:03:07 -0800

    Not sure how that happened

--- nserve-frontend/main.go
+++ nserve-frontend/main.go
@@ -78,6 +78,7 @@ var tmplText = `
 	</table>
 	{{end}}
 	</div>
+	{{end}}
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
