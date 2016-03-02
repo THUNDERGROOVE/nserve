fe885b60c843ea5e7257b88f5b60bd9739d2fef4
Nick Powell <markusthefish@gmail.com>
2016-03-01 19:18:59 -0800

    Fix it

--- nserve-frontend/main.go
+++ nserve-frontend/main.go
@@ -32,7 +32,7 @@ var tmplText = `
 </head>
 <body>
 
-<div class="navbar navbar-inverse">
+<div class="navbar navbar-inverse navbar-static-top">
 	<div class="navbar-header">
 		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 	  <span class="sr-only">Toggle navigation</span>
